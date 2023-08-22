63de9eff2ab9e0fe25cddec45978800b4ec21d16
cmotadev <cmota.dev@gmail.com>
2023-08-22 17:07:42 -0300

    [Docker] Rename some envs, start better readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# GeoServer Docker
+# GeoServer Container Image
+
+Uma implementação personalizada do GeoServer, compilado diretamente do código fonte
 
 Autor: Carlos Eduardo Mota
+
+## Build Args
+- TOMCAT_IMAGE_TAG=9-jre11-temurin-jammy
+- MAVEN_IMAGE_TAG=3.8-eclipse-temurin-11
+- MAVEN_OPTS="-Xmx512M"
+- GEOSERVER_VERSION=2.19.7 (testado com 2.17.x, 2.19.x, 2.21.x, 2.22.x, 2.23.x)
+- GEOSERVER_DATA_DIR=/srv/geoserver/data
+- GEOSERVER_EXTENSIONS=oracle,sqlserver,excel,app-schema,importer,jms-cluster,backup-restore,control-flow
+
+## Environment Variables
+- GEOSERVER_DATA_DIR=${GEOSERVER_DATA_DIR}
+- GEOSERVER_VERSION=${GEOSERVER_VERSION}
+- GEOSERVER_CONSOLE_DISABLED=FALSE
+- GEOSERVER_CSRF_DISABLED=TRUE
+- GEOSERVER_APPSCHEMA_PROPERTIES_FILE
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,13 +7,14 @@ GEOSERVER_OPTS="-Djava.awt.headless=true -server \
     -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 \
     -XX:InitiatingHeapOccupancyPercent=70 -XX:+CMSClassUnloadingEnabled \
     -Dorg.geotools.shapefile.datetime=true -Dgeoserver.login.autocomplete=off \
-    -DGEOSERVER_CONSOLE_DISABLED=${GEOSERVER_WEB_UI_DISABLED:-FALSE} \
+    -DGEOSERVER_CONSOLE_DISABLED=${GEOSERVER_CONSOLE_DISABLED:-FALSE} \
     -Xbootclasspath/a:$(find $CATALINA_HOME/webapps/geoserver -name marlin*.jar -print -quit) \
-    -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine"
+    -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine \
+    -DGEOSERVER_CSRF_DISABLED=${GEOSERVER_CSRF_DISABLED:-TRUE}"
 
-if [[ -n $APPSCHEMA_FILE ]]
+if [[ -n $GEOSERVER_APPSCHEMA_PROPERTIES_FILE ]]
 then
-    GEOSERVER_OPTS="$GEOSERVER_OPTS -Dapp-schema.properties=$APPSCHEMA_FILE"
+    GEOSERVER_OPTS="$GEOSERVER_OPTS -Dapp-schema.properties=$GEOSERVER_APPSCHEMA_PROPERTIES_FILE"
 fi
 
 exec env CATALINA_OPTS="${CATALINA_OPTS} ${GEOSERVER_OPTS}" "$@"
