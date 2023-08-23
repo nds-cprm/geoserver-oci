7b3e4fbbadce3b217e6618103bb9445aef58457f
cmotadev <cmota.dev@gmail.com>
2023-08-23 19:34:19 -0300

    [Entrypoint] Parametrização de CSRF, Marlin, Geowebcache,

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -ef -o pipefail
 
-GEOSERVER_OPTS="-Djava.awt.headless=true -server \
-    -XX:PerfDataSamplingInterval=500 -Dorg.geotools.referencing.forceXY=true \
-    -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:NewRatio=2 \
-    -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 \
-    -XX:InitiatingHeapOccupancyPercent=70 -XX:+CMSClassUnloadingEnabled \
-    -Dorg.geotools.shapefile.datetime=true -Dgeoserver.login.autocomplete=off \
-    -DGEOSERVER_CONSOLE_DISABLED=${GEOSERVER_CONSOLE_DISABLED:-FALSE} \
-    -Xbootclasspath/a:$(find $CATALINA_HOME/webapps/geoserver -name marlin*.jar -print -quit) \
-    -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine \
-    -DGEOSERVER_CSRF_DISABLED=${GEOSERVER_CSRF_DISABLED:-TRUE}"
+GEOSERVER_OPTS="-Djava.awt.headless=true \
+    -server \
+    -XX:PerfDataSamplingInterval=500 \
+    -XX:SoftRefLRUPolicyMSPerMB=36000 \
+    -XX:NewRatio=2 \
+    -XX:+UseG1GC \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:ParallelGCThreads=20 \
+    -XX:ConcGCThreads=5 \
+    -XX:InitiatingHeapOccupancyPercent=70 \
+    -XX:+CMSClassUnloadingEnabled \
+    -Dorg.geotools.referencing.forceXY=true \
+    -Dorg.geotools.shapefile.datetime=true \
+    -Dgeoserver.login.autocomplete=off \
+    -DGEOSERVER_CONSOLE_DISABLED=${GEOSERVER_CONSOLE_DISABLED:-FALSE}"
 
-if [[ -n $GEOSERVER_APPSCHEMA_PROPERTIES_FILE ]]
+# CSRF Whitelist
+GEOSERVER_CSRF_DISABLED=${GEOSERVER_CSRF_DISABLED:-FALSE}
+GEOSERVER_OPTS="$GEOSERVER_OPTS -DGEOSERVER_CSRF_DISABLED=${GEOSERVER_CSRF_DISABLED}"
+
+if [[ "$GEOSERVER_CSRF_DISABLED" == "FALSE" &&  -n $GEOSERVER_CSRF_WHITELIST ]]
+then
+    GEOSERVER_OPTS="$GEOSERVER_OPTS -DGEOSERVER_CSRF_WHITELIST=$GEOSERVER_CSRF_WHITELIST"
+fi
+
+# Marlin Rendering Engine
+GEOSERVER_DISABLE_MARLIN=${GEOSERVER_DISABLE_MARLIN:-FALSE}
+
+if [[ "$GEOSERVER_DISABLE_MARLIN" == "FALSE" ]]
+then
+    GEOSERVER_OPTS="$GEOSERVER_OPTS \
+        -Xbootclasspath/a:$(find $CATALINA_HOME/webapps/geoserver -name marlin*.jar -print -quit)\
+        -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine"
+fi
+
+# Geowebcache
+if [[ -n "$GEOWEBCACHE_CACHE_DIR" ]]
+then
+    if [[ -e "$GEOWEBCACHE_CACHE_DIR" && -w "$GEOWEBCACHE_CACHE_DIR" ]]
+    then
+        GEOSERVER_OPTS="$GEOSERVER_OPTS -DGEOWEBCACHE_CACHE_DIR=$GEOWEBCACHE_CACHE_DIR"
+    else
+        echo "The directory $GEOWEBCACHE_CACHE_DIR is not writable by user"
+        exit 1
+    fi
+fi
+
+# AppSchema Plugin - Properties file
+if [[ -r $GEOSERVER_APPSCHEMA_PROPERTIES_FILE ]]
 then
     GEOSERVER_OPTS="$GEOSERVER_OPTS -Dapp-schema.properties=$GEOSERVER_APPSCHEMA_PROPERTIES_FILE"
 fi
